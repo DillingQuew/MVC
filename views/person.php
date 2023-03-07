1cc0dbf5c1f7ac64b80dc7ff5919f0886e4568fc
Denis <daodawich@gmail.com>
2023-03-07 22:46:20 +0600

    add margin for button pay for month

--- views/person.php
+++ views/person.php
@@ -23,44 +23,7 @@
                 <input type="text" class="form-control" id="Salary" name="Salary" value="<?php echo($obj['Salary'])?>" placeholder="name@example.com">
 
 
-        <div style="display:none">
-            <div class="form-group" >
-                <label for="exampleFormControlSelect1">Example select</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            <div class="form-group" >
-                <label for="exampleFormControlSelect1">Example select</label>
-                <select class="form-control" id="exampleFormControlSelect1">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlSelect2">Example multiple select</label>
-                <select multiple class="form-control" id="exampleFormControlSelect2">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-        </div>
-
-        <button type="submit" class="btn btn-success" style="display: block">Зачислить зарплату</button>
+        <button type="submit" class="btn btn-success" style="display: block; margin: 20px 0;">Зачислить зарплату</button>
         </form>
         <?php
         }
